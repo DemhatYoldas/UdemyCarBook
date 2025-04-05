3c4a691e80225bcf3768e5dd0dd38a6c0a0844db
Demhat Yoldaş <64671581+DemhatYoldas@users.noreply.github.com>
2025-04-06 00:14:22 +0300

    Entity Tamamlanması

---
+++ Core/UdemyCarBook.Domain/Entities/Banner.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UdemyCarBook.Domain.Entities
+{
+    public class Banner
+    {
+        public int BannerId { get; set; }
+        public string Title { get; set; }
+        public string Descirption { get; set; }
+        public string VideoDescirption { get; set; }
+        public string VideoUrl { get; set; }
+    }
+}
---
+++ Core/UdemyCarBook.Domain/Entities/Testimonial.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UdemyCarBook.Domain.Entities
+{
+    internal class Testimonial
+    {
+        public int TestimonialID { get; set; }
+        public int Name  { get; set; }
+        public int Title { get; set; }
+        public int Commend { get; set; }
+        public int ImageUrl { get; set; }
+    }
+}
